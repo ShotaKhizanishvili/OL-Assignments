5fa9358683bb64173bfec1e752ede0b9ee085a91
ShotaKhizanishvili <shotiko.khizanishvili15@gmail.com>
2023-10-18 22:57:03 +0400

    Initial Commit

---
+++ assignments/Console Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Console_Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum, secondNum, result;
+            string operation;
+
+            result = default;
+
+            Console.WriteLine("Enter first number: ");
+            firstNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            secondNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter math operation (+, -, *, /): ");
+            operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    {
+                        result = Addition(firstNum, secondNum); break;
+                    }
+                case "-":
+                    {
+                        result = Subtraction(firstNum, secondNum); break;
+                    }
+                case "*":
+                    {
+                        result = Multiplication(firstNum, secondNum); break;
+                    }
+                case "/":
+                    {
+                        if (secondNum == 0)
+                        {
+                            Console.WriteLine("Can not divide by zero.");
+                            return;
+                        }
+                        result = Division(firstNum, secondNum); break;
+                    }
+                default:
+                    {
+                        Console.WriteLine(operation + " is not a math operation");
+                        break;
+                    }
+            }
+            Console.WriteLine(result);
+        }
+        public static double Addition(double firstNum,double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        public static double Subtraction(double firstNum, double secondNum)
+        {
+            return firstNum - secondNum;
+        }
+        public static double Multiplication(double firstNum, double secondNum)
+        {
+            return firstNum * secondNum;
+        }
+        public static double Division(double firstNum, double secondNum)
+        {
+            return firstNum / secondNum;
+        }
+    }
+}
---
+++ classroom/TemperatureConverter/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TemperatureConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, farenheit;
+            celsius = Convert.ToDouble(Console.ReadLine());
+            farenheit = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(celsius + " Celsius is " + CelsiusToF(celsius) + "F");
+            Console.WriteLine(farenheit + " Farenheit is " + FarenheitToC(farenheit) + "C");
+        }
+
+        //(0°C × 9/5) + 32 = 32°F
+        public static double CelsiusToF(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        //(0°F − 32) × 5/9 = -17.78°C
+        public static double FarenheitToC(double farenheit)
+        {
+            return (farenheit - 32) * 5 / 9;
+        }
+    }
+}
