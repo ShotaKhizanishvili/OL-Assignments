c7b74728ef958920dbceb63adbeab5acf318da83
ShotaKhizanishvili <shotiko.khizanishvili15@gmail.com>
2023-10-19 13:29:58 +0400

    Updated Console Calculator

--- assignments/Console Calculator/Program.cs
+++ assignments/Console Calculator/Program.cs
@@ -4,50 +4,53 @@
     {
         static void Main(string[] args)
         {
-            double firstNum, secondNum, result;
-            string operation;
-
-            result = default;
-
-            Console.WriteLine("Enter first number: ");
-            firstNum = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter second number: ");
-            secondNum = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter math operation (+, -, *, /): ");
-            operation = Console.ReadLine();
-
-            switch (operation)
+            while (true)
             {
-                case "+":
-                    {
-                        result = Addition(firstNum, secondNum); break;
-                    }
-                case "-":
-                    {
-                        result = Subtraction(firstNum, secondNum); break;
-                    }
-                case "*":
-                    {
-                        result = Multiplication(firstNum, secondNum); break;
-                    }
-                case "/":
-                    {
-                        if (secondNum == 0)
+                double firstNum, secondNum, result;
+                string operation;
+
+                result = default;
+
+                Console.Write("Enter first number:");
+                firstNum = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter second number:");
+                secondNum = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter math operation (+, -, *, /):");
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
                         {
-                            Console.WriteLine("Can not divide by zero.");
-                            return;
+                            result = Addition(firstNum, secondNum); break;
                         }
-                        result = Division(firstNum, secondNum); break;
-                    }
-                default:
-                    {
-                        Console.WriteLine(operation + " is not a math operation");
-                        break;
-                    }
+                    case "-":
+                        {
+                            result = Subtraction(firstNum, secondNum); break;
+                        }
+                    case "*":
+                        {
+                            result = Multiplication(firstNum, secondNum); break;
+                        }
+                    case "/":
+                        {
+                            if (secondNum == 0)
+                            {
+                                Console.WriteLine("Can not divide by zero.");
+                                return;
+                            }
+                            result = Division(firstNum, secondNum); break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine(operation + " is not a math operation");
+                            break;
+                        }
+                }
+                Console.WriteLine($"{firstNum} {operation} {secondNum} = {result}");
             }
-            Console.WriteLine(result);
         }
         public static double Addition(double firstNum,double secondNum)
         {
