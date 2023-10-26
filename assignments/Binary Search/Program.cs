623afcfd3b03d7dcf7e82b78535edb85fb30b161
ShotaKhizanishvili <shotiko.khizanishvili15@gmail.com>
2023-10-26 19:58:50 +0400

    added 3rd and 4th assignments

---
+++ assignments/Binary Search/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 0, 1, 5, 76, 234, 678 };
+            var target = 76;
+
+            var index = BinarySearch(array, target);
+            Console.WriteLine(index); // index უნდა იყოს 3
+
+            index = BinarySearch(array, 75);
+            Console.WriteLine(index); // index უნდა იყოს -1
+        }
+        public static int BinarySearch(int[] arr, int target)
+        {
+            int min = 0;
+            int max = arr.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < target)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+            return -1;
+        }     
+    }
+}
---
+++ assignments/Vowel or not/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Vowel_or_not
+{
+    internal class Program
+    {
+        static char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'ა', 'ე', 'ი', 'ო', 'უ' };
+        static void Main(string[] args)
+        {
+            var isVowel = IsVowel('a'); // true
+            Console.WriteLine(isVowel);
+
+            isVowel = IsVowel('b'); // false
+            Console.WriteLine(isVowel);
+
+            isVowel = IsVowel('ე'); // true
+            Console.WriteLine(isVowel);
+
+            isVowel = IsVowel('კ'); // false
+            Console.WriteLine(isVowel);
+        }
+        public static bool IsVowel(char c)
+        {
+            if (vowels.Contains(c))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
