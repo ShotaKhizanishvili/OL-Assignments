b836ce57062b5795437ff9744340e33cef64be57
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2023-10-26 18:06:06 +0400

    added classroom assignments

---
+++ classroom/Even Number Finder/Program.cs
@@ -0,0 +1,32 @@
+﻿//Even Number Finder: (Medium)Build a program that finds all even numbers from 1 to n,
+//    utilizing the continue statement
+
+namespace Even_Number_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!IsEven(i))
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+
+            }
+        }
+        public static bool IsEven(int k)
+        {
+            if (k % 2 == 0 && k != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ classroom/Number Printer/Program.cs
@@ -0,0 +1,19 @@
+﻿//Number Printer: (Easy)Create a program that prints numbers from 1 to n,
+//    where n is input by the user
+
+namespace Number_Printer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
