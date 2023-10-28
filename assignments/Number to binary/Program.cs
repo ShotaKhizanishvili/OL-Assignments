bf9229288919f8b673ec75332b6f8a22749cab4b
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2023-10-28 22:58:23 +0400

    added 5th assignment

---
+++ assignments/Number to binary/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Number_to_binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            long number = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine(ToBinary(number));
+        }
+
+        public static string ToBinary(long number)
+        {
+            return Convert.ToString(number, 2);
+        }
+    }
+}
