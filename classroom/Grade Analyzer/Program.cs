026b49efe8eb67ff84092e7ef039e9b882ade352
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2023-10-31 17:49:41 +0400

    added classroom assignments

---
+++ classroom/Grade Analyzer/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Grade_Analyzer
+{
+    internal class Program
+    {
+        public enum LetterGrade { A, B, C, D, F }
+
+        static void Main(string[] args)
+        {
+            //...
+            int[] gradesArr = new int[1];
+
+            Console.Write("Enter your grades separated by commas: ");
+
+            Console.Write($"Average Grade: {CalculateAverage(gradesArr)}");
+
+            Console.Write($"Average Grade: {DetermineLetterGrade()}");
+        }
+
+        public static double CalculateAverage(int[] gradesArr)
+        {
+            return default;
+        }
+        public static LetterGrade DetermineLetterGrade()
+        {
+            return default;
+        }
+    }
+}
---
+++ classroom/Investment Forecaster/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Investment_Forecaster
+{
+    internal class Program
+    {
+        private static double compound = 1;
+        static void Main(string[] args)
+        {
+            decimal pValue;
+            double rate, timeInYears;
+
+            Console.Write("Enter the initial principal amount: ");
+            pValue = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter the annual interest rate (as a percentage): ");
+            rate = Convert.ToDouble(Console.ReadLine())/100;
+
+            Console.Write("Enter the number of years: ");
+            timeInYears = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Future value of your investment: ${CalculateFutureValue(pValue, rate, timeInYears).ToString("N2")}");
+        }
+        public static decimal CalculateFutureValue(decimal pValue, double rate, double timeInYears) //FV = P * (1 + (r / n))^(n * t)
+        {
+            return pValue * Convert.ToDecimal(Math.Pow(1 + (rate / compound), compound * timeInYears));
+        }
+    }
+}
