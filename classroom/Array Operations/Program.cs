cd919910343ef592d55f5767d2f3dbbf00386ab6
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2023-11-07 15:08:50 +0400

    added classroom assignments

---
+++ classroom/Array Operations/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Array_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your numbers separated by commas: ");
+            string input = Console.ReadLine();
+            string[] numberStrings = input.Split(',');
+            double[] numbers = new double[numberStrings.Length];
+
+            for (int i = 0; i < numberStrings.Length; i++)
+            {
+                string trimmedNumString = numberStrings[i].Trim();
+                double.TryParse(trimmedNumString, out double value);
+                numbers[i] = value;
+            }
+
+            Console.WriteLine($"Sum: {CalculateSum(numbers)}");
+            Console.WriteLine($"Average: {CalculateAverage(numbers)}");
+            Console.WriteLine($"Maximum: {FindMax(numbers)}");
+            Console.WriteLine($"Minimum: {FindMin(numbers)}");
+        }
+        public static double CalculateSum(double[] numbers)
+        {
+            double sum = 0;
+            foreach (double number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+        public static double CalculateAverage(double[] numbers)
+        {
+            return CalculateSum(numbers) / numbers.Length;
+        }
+        public static double FindMax(double[] numbers)
+        {
+            double maxValue = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxValue)
+                {
+                    maxValue = numbers[i];
+                }
+            }
+            return maxValue;
+        }
+        public static double FindMin(double[] numbers)
+        {
+            double minValue = numbers[0];
+            for (int i = 0;i<numbers.Length;i++)
+            {
+                if (numbers[i] < minValue)
+                {
+                    minValue = numbers[i];
+                }
+            }
+            return minValue;
+        }
+    }
+}
---
+++ classroom/Seat Reservation/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Seat_Reservation
+{
+    internal class Program
+    {
+        private static char[,] seats = new char[4, 4] {
+            { '0','0','0','0' },
+            { '0','0','0','0' },
+            { '0','0','0','0' },
+            { '0','0','0','0' }
+        };
+        static void Main(string[] args)
+        {
+            int choice;
+
+            Console.WriteLine("Initial Seating Chart:");
+            DisplaySeats();
+
+            Console.WriteLine("Select an option: ");
+            Console.WriteLine("1) Reserve a seat");
+            Console.WriteLine("2)View available seats");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice) 
+            {
+                case 1:break;
+                case 2:break;
+                default:break;
+            }
+        }
+        public static void DisplaySeats()
+        {
+            for (int i = 0; i < seats.GetLength(0); i++)
+            {
+                for (int k = 0; k < seats.GetLength(1); k++)
+                {
+                    Console.Write(seats[i, k]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void ReserveSeat()
+        {
+            int index1, index2;
+            Console.Write("Enter the row number: ");
+            index1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the column number: ");
+            index2 = Convert.ToInt32(Console.ReadLine());
+            if (index1 >= seats.GetLength(0) || index2 >= seats.GetLength(1))
+            {
+                Console.WriteLine("You entered wrong seats, try again.");
+            }
+
+            seats[index1, index2] = 'X';
+            Console.WriteLine("Updated Seating Chart: ");
+            DisplaySeats();
+        }
+        public static void AvailableSeats()
+        {
+            DisplaySeats();
+        }
+    }
+}
