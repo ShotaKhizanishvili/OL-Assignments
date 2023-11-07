dd9f98e73f74021935fd7ec36a45390686a460e1
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2023-11-07 17:47:38 +0400

    added classroom assignments

---
+++ classroom/Palindrome Checker/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Palindrome_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string input;
+
+                Console.WriteLine("Please enter a string to check if it's a palindrome:");
+                input = Console.ReadLine();
+
+                if (input == string.Empty)
+                {
+                    throw new ArgumentException("The string is empty, try again.");
+                }
+                if (IsPalindrome(input))
+                {
+                    Console.WriteLine("The string is palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine("The string is not a palindrome.");
+                }
+            }
+            while (Prompt());
+        }
+
+        private static bool Prompt()
+        {
+            string answer;
+            Console.WriteLine("Would you like to check another string? (yes/no):");
+            answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            Console.WriteLine("Checking...");
+            input = input.Replace(",", "").Replace(" ", "").ToLower();
+            if (input == Reverse(input))
+            {
+                return true;
+            }
+            return false;
+        }
+        private static string Reverse(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ classroom/Text Analyzer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Text_Analyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Please enter a block of text for analysis:");
+            input = Console.ReadLine().ToLower();
+            string[] inputStrings = input.Split(',');
+            for (int i = 0; i < inputStrings.Length; i++)
+            {
+                inputStrings[i] = inputStrings[i].Trim();
+            }
+        }
+        public static void CountWords(string[] arr)
+        {
+
+        }
+        public static void FindMostCommonWord()
+        {
+
+        }
+        public static void FindLongestWord()
+        {
+
+        }
+        private static void arrayCleaner(string[] arr)
+        {
+
+        }
+    }
+}
