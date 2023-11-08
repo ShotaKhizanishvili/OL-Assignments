7e144f057629456262bf7c054bb8cc0f27936cfa
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2023-11-08 17:52:16 +0400

    added classroom assignments

---
+++ classroom/Person Class/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Person_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Alice", 30);
+            Person person2 = new Person("Bob", 25);
+            person1.Greet();
+            person2.Greet();
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            if (age > 0)
+            {
+                Age = age;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Age must be more than 0.");
+            }
+        }
+        public bool IsAdult()
+        {
+            if (this.Age >= 18)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Greet()
+        {
+            Console.WriteLine(this.ToString());
+        }
+        public override string ToString()
+        {
+            return $"Hello, my name is {this.Name} and I am {this.Age} years old.";
+        }
+    }
+}
---
+++ classroom/Simple Bank Account/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Simple_Bank_Account
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount(1000);
+            account.Deposit(500);
+            account.Withdraw(2000);
+            account.Withdraw(100);
+
+            Console.WriteLine(account.ToString());
+        }
+    }
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        public BankAccount(decimal balance)
+        {
+            Console.WriteLine($"Initializing new bank account with a balance of ${balance}.");
+            _balance = balance;
+        }
+
+        public void Deposit(decimal funds)
+        {
+            if (funds <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Funds for deposit must be more than 0.");
+            }
+            Console.WriteLine($"Depositing ${funds}...");
+            this._balance += funds;
+            Console.WriteLine($"New balance is ${this._balance}");
+        }
+        public void Withdraw(decimal funds)
+        {
+            if (funds <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Funds for withdraw must be more than 0.");
+            }
+            if (funds > this._balance)
+            {
+                Console.WriteLine("Insufficient funds. Transaction denied.");
+                return;
+            }
+            Console.WriteLine($"Withdrawing {funds}");
+            this._balance -= funds;
+            Console.WriteLine($"New balance is ${this._balance}.");
+        }
+        public override string ToString()
+        {
+            return $"Current balance: ${this._balance}";
+        }
+    }
+
+}
