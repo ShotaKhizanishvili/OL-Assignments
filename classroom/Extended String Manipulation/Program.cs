9196d8480c4c40bea40454600b7f74bfd525f72d
ShotaKhizanishvili <shotiko.khizanishvili15@gmail.com>
2023-12-06 16:41:26 +0400

    added classroom assignment

---
+++ classroom/Extended String Manipulation/Program.cs
@@ -0,0 +1,8 @@
+﻿using Extended_String_Manipulation;
+
+string originalString = "Lorem ipsum dolor sit amet";
+
+Console.WriteLine($"Original String: \"{originalString}\"");
+Console.WriteLine($"Reversed: \"{originalString.Reverse()}\"");
+Console.WriteLine($"Title Case: \"{originalString.ToTitleCase()}\"");
+Console.WriteLine($"Word Count: {originalString.WordCount()}");
---
+++ classroom/Extended String Manipulation/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Extended_String_Manipulation;
+
+public static class StringExtensions
+{
+    public static string Reverse(this string input)
+    {
+        var charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public static string ToTitleCase(this string input)
+    {
+        var words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] letters = words[i].ToCharArray();
+            letters[0] = char.ToUpper(letters[0]);
+            for (int j = 1; j < letters.Length; j++)
+            {
+                letters[j] = char.ToLower(letters[j]);
+            }
+
+            words[i] = new string(letters);
+        }
+
+        return string.Join(" ", words);
+    }
+
+    public static int WordCount(this string input)
+    {
+        var words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
