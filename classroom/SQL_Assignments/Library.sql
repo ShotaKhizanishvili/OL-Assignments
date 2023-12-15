ab117605bb6c10ed9e8d72dc6484288f7f8d5aad
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2023-12-15 18:07:13 +0400

    added changes in Library.sql

--- classroom/SQL_Assignments/Library.sql
+++ classroom/SQL_Assignments/Library.sql
@@ -5,21 +5,21 @@ USE Library;
 GO
 
 CREATE TABLE Authors (
-    AuthorId INT PRIMARY KEY,
+    AuthorId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
     Age INT
 );
 
 CREATE TABLE Books (
-    BookId INT PRIMARY KEY,
+    BookId INT IDENTITY(1,1) PRIMARY KEY,
     Title NVARCHAR(100) NOT NULL,
     AuthorId INT,
-    YearPublished DATE,
+    YearPublished INT,
     FOREIGN KEY (AuthorId) REFERENCES Authors(AuthorId)
 );
 
 CREATE TABLE Members (
-    MemberId INT PRIMARY KEY,
+    MemberId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
     MembershipDate DATE
 );
@@ -32,3 +32,46 @@ FROM Authors A
 JOIN Books B ON A.AuthorId = B.AuthorId
 GROUP BY A.AuthorId, A.Name
 HAVING COUNT(B.BookId) > 3;
+
+INSERT INTO Members(Name,MembershipDate)
+VALUES
+	('name1','2001-01-01'),
+	('name2','2002-02-02'),
+	('name3','2003-03-03'),
+	('name4','2004-04-04'),
+	('name5','2005-05-05')
+;
+
+INSERT INTO Authors(Name,Age)
+VALUES
+	('name1',18),
+	('name2',19),
+	('name3',20),
+	('name4',21),
+	('name5',22)
+;
+
+INSERT INTO Books(Title,AuthorId,YearPublished)
+VALUES
+	('bookname1',1,2001),
+	('bookname2',1,2002),
+	('bookname3',2,2003),
+	('bookname4',3,2004),
+	('bookname5',5,2005)
+;
+
+UPDATE Authors
+SET Age = 41
+WHERE Age = 20;
+
+DELETE FROM Books
+WHERE BookId = 2;
+
+SELECT * FROM Authors
+ORDER BY Name;
+
+SELECT * FROM Books
+WHERE YearPublished BETWEEN 1990 AND 2003;
+
+SELECT BookId,Title,YearPublished,COUNT(AuthorId) as BookCount FROM Books
+GROUP BY BookId,Title,YearPublished;
