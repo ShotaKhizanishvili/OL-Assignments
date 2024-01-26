014277e9c2cfce278dac0b85010a39072eabf448
Shota Khizanishvili <shotiko.khizanishvili15@gmail.com>
2024-01-26 17:54:03 +0400

    added classroom assignment

---
+++ classroom/ApiCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Addition")]
+        public IActionResult Addition(decimal firstNum, decimal secondNum)
+        {
+            var result = firstNum + secondNum;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("Subtraction")]
+        public IActionResult Subtraction(decimal firstNum, decimal secondNum)
+        {
+            var result = firstNum - secondNum;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("Multiplication")]
+        public IActionResult Multiplication(decimal firstNum, decimal secondNum)
+        {
+            var result = firstNum * secondNum;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("Division")]
+        public IActionResult Division(decimal firstNum, decimal secondNum)
+        {
+            if (secondNum == 0)
+            {
+                throw new ArgumentException("can't divide by zero.");
+            }
+            var result = firstNum / secondNum;
+            return Ok(result);
+        }
+    }
+}
