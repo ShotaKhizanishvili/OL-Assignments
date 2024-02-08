b7ab84aa6ed0c06fe9f2215e54d796ae45de1c92
ShotaKhizanishvili <shotiko.khizanishvili15@gmail.com>
2024-02-08 13:39:01 +0400

    added assignment Move element to end

---
+++ assignments/Move element to end/Program.cs
@@ -0,0 +1,27 @@
+﻿var array = new List<int>() { 2, 1, 2, 2, 2, 3, 4, 2 };
+var toMove = 2;
+
+MoveElements(array, toMove);
+
+Console.WriteLine(string.Join(", ", array));
+
+static void MoveElements(List<int> array, int toMove)
+{
+    int leftPointer = 0;
+    int rightPointer = array.Count - 1;
+
+    while (leftPointer < rightPointer)
+    {
+        if (array[leftPointer] == toMove)
+        {          
+            int temp = array[leftPointer];
+            array[leftPointer] = array[rightPointer];
+            array[rightPointer] = temp;
+            rightPointer--;
+        }
+        else
+        {
+            leftPointer++;
+        }
+    }
+}
