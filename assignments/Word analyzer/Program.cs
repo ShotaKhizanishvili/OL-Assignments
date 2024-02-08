57bb8091d93ad59e13c9767358207916e7122d0a
ShotaKhizanishvili <shotiko.khizanishvili15@gmail.com>
2024-02-08 14:01:47 +0400

    added assignment Word analyzer

---
+++ assignments/Word analyzer/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] words = File.ReadAllLines("C:\\Users\\Shota\\Desktop\\OL-Assignments\\assignments\\Word analyzer\\words_alpha.txt");
+
+string shortestWord = words.OrderBy(w => w.Length).FirstOrDefault();
+string longestWord = words.OrderByDescending(w => w.Length).FirstOrDefault();
+
+Console.WriteLine($"Shortest word: {shortestWord}, Length: {shortestWord.Length}");
+Console.WriteLine($"Longest word: {longestWord}, Length: {longestWord.Length}");
+
+var wordsWithVowels = words.Select(w => new { Word = w, VowelCount = CountVowels(w) })
+                           .OrderByDescending(w => w.VowelCount)
+                           .Take(100)
+                           .Select(w => w.Word);
+
+Console.WriteLine("100 words with the most vowels:");
+foreach (var word in wordsWithVowels)
+{
+    Console.WriteLine(word);
+}
+
+static int CountVowels(string word)
+{
+    char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+    return word.Count(c => vowels.Contains(c));
+}
